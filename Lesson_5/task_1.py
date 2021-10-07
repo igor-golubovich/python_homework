bae6191fabf2f2682ef91f9ae79ad671f093c76b
igor_golubovich <igoz_666@mail.ru>
2021-10-07 18:24:45 +0300

    hm 5_1 6_1

--- Lesson_5/task_1.py
+++ Lesson_5/task_1.py
@@ -1,98 +1,42 @@
 # Golubovich Igor for Python Course | homework №5 | task №1
 
-# def input_type(message):
-#     while True:
-#         try:
-#             userInput1 = int(input(message))
-#         except ValueError:
-#             print('!!!ERROR!!! Need to enter the number from 1 to 5 !!!')
-#             continue
-#         if userInput1 < 1:
-#             print('!!!ERROR!!! Need to enter the number from 1 to 5 !!!')
-#             continue
-#         elif 5 < userInput1:
-#             print('!!!ERROR!!! Need to enter the number from 1 to 5 !!!')
-#             continue
-#         else:
-#             return userInput1
+def validation(func):
+    def wrapper(type_arg, *args):
+        result = []
+        if type_arg == str:
+            for i in args:
+                i = str(i)
+                result.append(i)
+            return func(type_arg, *result)
+        elif type_arg == int:
+            for i in args:
+                i = int(i)
+                result.append(i)
+            return func(type_arg, *result)
+        elif type_arg == float:
+            for i in args: 
+                i = float(i)
+                result.append(i)
+            return func(type_arg, *result)
+        elif type_arg == list:
+            for i in args:
+                result.append(i)
+            return func(type_arg, result)
+        elif type_arg == tuple:
+            for i in args:
+                result.append(i)
+            result = tuple(result)
+            return func(type_arg, result)
+    return wrapper
 
 
 
-# def validation(fnc):
-#     def wrapper(type_f, inp_value):
-#         inp_value = []
-#         if type_f == 1:
-#             for i in inp_value:
-#                 i = int(i)
-#                 print(type(i))
-#                 inp_value.append(i)
-#             return fnc(type_f, inp_value)
-#     return wrapper
-            
+@validation
+def data_change_type(type_arg, *args):
+    return args
 
-
-# @validation
-# def inp_f(type_f, inp_value):
-#     return inp_value
-
-
-# x =  input('Enter a list of values:\n')
-# y = input_type('Enter a number to define the data type:\n1 - int\n2 - float\n3 - str\n4 - typle\n5 - list\nInput number:  ')
-
-# a = inp_f(x, y)
-
-
-# __________________________________________________________
-
-dic_value = {
-    1 : int,
-    2 : float,
-    3 : str,
-    4 : tuple,
-    5 : list,
-}
-
-def input_type(message):
-    while True:
-        try:
-            userInput1 = int(input(message))
-        except ValueError:
-            print('!!!ERROR!!! Need to enter the number from 1 to 5 !!!')
-            continue
-        if userInput1 < 1:
-            print('!!!ERROR!!! Need to enter the number from 1 to 5 !!!')
-            continue
-        elif 5 < userInput1:
-            print('!!!ERROR!!! Need to enter the number from 1 to 5 !!!')
-            continue
-        else:
-            return userInput1
-
-
-
-# def validation_1(type_arg):
-#     def actual_decorator(func):       
-#         def wrapper(x):
-#             x = type_arg(x)
-#             return func(x)
-#         return wrapper
-#     return actual_decorator
-
-# x = input('Enter a list of values:\n')
-z = int(input_type('Enter a number to define the data type:\n1 - int\n2 - float\n3 - str\n4 - tuple\n5 - list\nInput number: '))
-y = (dic_value.get(z))
-print(y)
-
-
-# @validation_1(type_arg = y)
-# def inp_f(x):
-#     return a
-
-
-
-# a = inp_f(x)
-# print(a)
-# print(type(a))
+a = data_change_type(int, 9.5,8,78,5.56)
+print(a)
 
 
 
--- Lesson_6/task_6_1.py
+++ Lesson_6/task_6_1.py
@@ -9,26 +9,34 @@ class Compl:
         self.c2 = complex(c2)
 
     def summ_c(self):
-        return self.c1 + self.c2 
+        return f'summ = {self.c1 + self.c2}'
 
     def sub_c(self):
-        return self.c1 - self.c2 
+        return f'sub = {self.c1 - self.c2}'
 
     def mul_c(self):
-        return self.c1 * self.c2
+        return f'mul = {self.c1 * self.c2}'
     
     def div_c(self):
-        return self.c1 / self.c2
+        return f'div = {self.c1 / self.c2}'
 
     def mod_c(self):
-        return abs(self)
+        a = abs(self.c1)
+        b = abs(self.c2)
+        return f'mod_c1 = {a}\nmod_c2 = {b}'
 
-    def __repr__(self):
-        return f'summ = {self.c1 + self.c2}\nsub = {self.c1 - self.c2}\nmul = {self.c1 * self.c2}\ndiv = {self.c1 / self.c2}\nmod_c1 = {abs(self.c1)}\nmod_c2 = {abs(self.c2)}'
+    # def __repr__(self):
+    #     return f'summ = {self.c1 + self.c2}\nsub = {self.c1 - self.c2}\nmul = {self.c1 * self.c2}\ndiv = {self.c1 / self.c2}\nmod_c1 = {abs(self.c1)}\nmod_c2 = {abs(self.c2)}'
+
+    # def __repr__(self):
+    #     return self.summ_c()
 
         
 
-
 p = Compl(1 + 2j, 2 + 4j)
-print(p.summ_c)
-#print(p)
+print(p.summ_c())
+print(p.sub_c())
+print(p.mul_c())
+print(p.div_c())
+print(p.mod_c())
+# print(p)
